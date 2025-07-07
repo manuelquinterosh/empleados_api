e47ed59ba10d3acdfa24acee5999187a2fa958b1
Manuel Quinteros <manuelq_hernandez@outlook.com>
2025-07-07 00:20:55 -0600

    refactor: integrar middlewares y manejo global de errores en app.js

--- app.js
+++ app.js
@@ -19,7 +19,4 @@ app.use((req, res) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor http://localhost:${PORT}`);
-})
-
+module.exports = app;
