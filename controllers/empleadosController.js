a4c017527f183d05ad8b89b1a5001e2b922337e4
Manuel Quinteros <manuelq_hernandez@outlook.com>
2025-07-07 02:05:17 -0600

    chore: eliminar codigo comentado innecesario

--- controllers/empleadosController.js
+++ controllers/empleadosController.js
@@ -10,17 +10,6 @@ const {
 const validarEmpleado = require('../utils/validarEmpleado');
 
 const crearEmpleado = async (req, res) => {
-    //const { nombre, edad, puesto, departamento } = req.body;
-
-    /**if (!nombre || nombre.length < 3) {
-        return res.status(400).json({ error: 'El nombre debe tener al menos 3 caracteres.'});
-    }
-    if (!Number.isInteger(edad) || edad <= 0){
-        return res.status(400).json({ error: 'La edad debe ser un numero entero positivo.'});
-    }
-    if (!puesto || !departamento) {
-        return res.status(400).json({ error:'Puesto y departamento son requeridos.'});
-    }**/
    const errores = validarEmpleado(req.body);
 
    if (errores.length > 0) {
@@ -57,14 +46,8 @@ const actualizarEmpleadoController = async (req, res) => {
     const { id } = req.params;
     const datos = req.body;
 
-    /*if (datos.nombre && datos.nombre.length < 3) {
-        return res.status(400).json({ error: 'El nombre debe tener al menos 3 caracteres.'});
-    }
+    const errores = validarEmpleado(datos, true);
 
-    if (datos.edad !== undefined && (!Number.isInteger(datos.edad) || datos.edad <= 0)){
-        return res.status(400).json({ error: 'La edad debe ser un numero entero positivo.'});
-    }*/
-   const errores = validarEmpleado(datos, true);
    if (errores.length > 0) {
     return res.status(400).json({ error: errores });
    }
